8076c385b5dd47d5388dbf7ae6a6bfe8bb21e4a4
Yura Murashko <yourass@internet.ru>
2025-01-23 20:26:53 +0300

    Add logger

--- src/main/java/ru/yourass/shoplist/ShopListController.java
+++ src/main/java/ru/yourass/shoplist/ShopListController.java
@@ -1,10 +1,19 @@
 package ru.yourass.shoplist;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import jakarta.annotation.PostConstruct;
 
 @Controller
 public class ShopListController {
+    private static final Logger logger = LoggerFactory.getLogger(ShopListController.class);
+    
+    @PostConstruct
+    public void init() {
+        logger.info("init");
+    }
     
     @GetMapping("/")
     public String hello() {
