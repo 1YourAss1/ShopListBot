f2e0c25fc965133cda6ebcb096591936b7121ce2
Yura Murashko <yourass@internet.ru>
2025-11-22 01:18:56 +0300

    Add empty item to lists

--- src/main/resources/static/js/listLoader.js
+++ src/main/resources/static/js/listLoader.js
@@ -4,7 +4,9 @@
     const initData = tg.initData;
 
     const shopList = document.getElementById('shop-list');
+    const shopListEmptyItem = document.getElementById('shop-list-empty-item');
     const shopListCompleted = document.getElementById('shop-list-completed');
+    const shopListCompletedEmptyItem = document.getElementById('shop-list-completed-empty-item');
 
     const overlay = document.getElementById('overlay');
     const completedPanel = document.getElementById('completedPanel');
@@ -34,54 +36,45 @@
             });
             const purchases = await response.json();
 
-            shopList.innerHTML = '';
-            shopListCompleted.innerHTML = '';
-
-            if (purchases.length === 0) {
+            purchases.forEach((purchase) => {
                 const shopItem = document.createElement('li');
                 shopItem.classList.add('item');
-                shopItem.innerHTML = `<label class="item-text">Пусто &#128549;</label></li>`;
-                shopList.appendChild(shopItem);
-            } else {
-                purchases.forEach((purchase) => {
-                    const shopItem = document.createElement('li');
-                    shopItem.classList.add('item');
-                    shopItem.innerHTML = `
-                                <input id="${purchase.id}" type="checkbox" class="checkbox" ${purchase.completed ? 'checked' : ''}>
-                                <label for="${purchase.id}" class="item-text">${purchase.product.name + getQuantityStr(purchase.quantity)}</label>
-                            `;
-                    shopItem.addEventListener('change', async (event) => {
-                        const checkbox = event.target;
-                        const item = checkbox.closest('li');
-                        const checked = checkbox.checked;
-                        const checkedBefore = !checkbox.checked;
+                shopItem.innerHTML = `
+                            <input id="${purchase.id}" type="checkbox" class="checkbox" ${purchase.completed ? 'checked' : ''}>
+                            <label for="${purchase.id}" class="item-text">${purchase.product.name + getQuantityStr(purchase.quantity)}</label>
+                        `;
+                shopItem.addEventListener('change', async (event) => {
+                    const checkbox = event.target;
+                    const item = checkbox.closest('li');
+                    const checked = checkbox.checked;
+                    const checkedBefore = !checkbox.checked;
 
-                        moveItem(item, checked);
-                        const response = await fetch(`api/toggle`, {
-                            method: 'PATCH',
-                            headers: {
-                                'Authorization': `tma ${initData}`,
-                                'Content-Type': 'application/json'
-                            },
-                            body: JSON.stringify({
-                                id: checkbox.id,
-                                completed: checked
-                            })
-                        });
-                        if (!response.ok) {
-                            checkbox.checked = checkedBefore;
-                            moveItem(item, checkedBefore)
-                            console.error('Не удалось обновить статус');
-                        }
+                    moveItem(item, checked);
+                    const response = await fetch(`api/toggle`, {
+                        method: 'PATCH',
+                        headers: {
+                            'Authorization': `tma ${initData}`,
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({
+                            id: checkbox.id,
+                            completed: checked
+                        })
                     });
-
-                    if (purchase.completed) {
-                        shopListCompleted.appendChild(shopItem);
-                    } else {
-                        shopList.appendChild(shopItem);
+                    if (!response.ok) {
+                        checkbox.checked = checkedBefore;
+                        moveItem(item, checkedBefore)
+                        console.error('Не удалось обновить статус');
                     }
                 });
-            }
+
+                if (purchase.completed) {
+                    shopListCompleted.appendChild(shopItem);
+                } else {
+                    shopList.appendChild(shopItem);
+                }
+            });
+            updateEmptyItems();
         } catch (error) {
             console.log(error);
         }
@@ -99,13 +92,23 @@
         li.classList.toggle('checked', completed);
         li.querySelector('.checkbox')?.classList.toggle('checked', completed);
 
-        li.remove(); // убираем из текущего списка
+        li.remove();
 
         if (completed) {
             shopListCompleted.appendChild(li);
         } else {
             shopList.appendChild(li);
         }
+
+        updateEmptyItems();
+    }
+
+    function updateEmptyItems() {
+        const hasActive = shopList.children.length > 1;
+        const hasCompleted = shopListCompleted.children.length > 1;
+
+        shopListEmptyItem.style.display = hasActive ? 'none' : 'flex';
+        shopListCompletedEmptyItem.style.display = hasCompleted ? 'none' : 'flex';
     }
 
     tg.ready();
